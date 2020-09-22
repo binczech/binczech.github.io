cfbef8060c31823cab3bc770e6e474c8f79667a4
Tadeas Pavlik <Pavlik.Tadeas@seznam.cz>
2020-09-22 09:57:38 +0200

    Updates

---
+++ precache-manifest.130f37395840c686b1cb7aa040ff8497.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547ea8dd61c8de0e19b8ce46e1f7910e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc61a936ecfc78dd8bd5",
+    "url": "/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "acf0a623694ebe4db87b",
+    "url": "/static/js/2.7fe6e9a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7fe6e9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc61a936ecfc78dd8bd5",
+    "url": "/static/js/main.89518bec.chunk.js"
+  },
+  {
+    "revision": "04e68ca394a9a9f4c20e",
+    "url": "/static/js/runtime-main.01e21733.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
